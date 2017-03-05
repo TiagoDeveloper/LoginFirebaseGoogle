f4e4e97fbcfca7eeaa4f76752e0c0a9992de7ae3
Tiago Fonseca <tiagopereirafonseca@gmail.com>
2017-03-05 15:46:38 -0300

    login usando conta do google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from './providers/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+    private isLoggedIn: boolean;
+    private user_displayName: string;
+    private user_email: string;
+
+    constructor(public authService: AuthService, private router: Router) {
+      this.authService.af.auth.subscribe(auth =>{
+        if(auth == null) {
+          // not logged in
+          this.isLoggedIn = false;
+          this.user_displayName = '';
+          this.user_email = '';
+          this.router.navigate(['login']);
+        } else {
+          // logged in
+          this.isLoggedIn = true;
+          this.user_displayName = auth.google.displayName;
+          this.user_email = auth.google.email;
+          this.router.navigate(['']);
+        }
+      });
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AuthService } from './providers/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+    apiKey: 'AIzaSyCvQol77iIE1IGf0shtk-4__Hm9HbQqmls',
+    authDomain: 'appweb-7dc7d.firebaseapp.com',
+    databaseURL: 'https://appweb-7dc7d.firebaseio.com',
+    storageBucket: 'appweb-7dc7d.appspot.com',
+    messagingSenderId: '150630602931'
+};
+
+const appRoutes: Routes = [
+  { path: '', component: HomePageComponent },
+  { path: 'login', component: LoginPageComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
